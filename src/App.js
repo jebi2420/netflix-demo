a6ec43d31468526aa909e068ead722283b33ec38
jebi2420 <jebi2420@gmail.com>
2024-04-25 17:26:07 +0900

    [add] 나머지 페이지들 라우트 등록

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { Route } from 'react-router-dom';
 import './App.css';
 import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import Movies from './pages/Movies/Movies';
 
-// 홈페이지
-// 영화 전체 보여주는 페이지 (+ 서치)
-// 영화 디테일 페이지
+// 홈페이지 / 
+// 영화 전체 보여주는 페이지 (+ 서치) /movies
+// 영화 디테일 페이지 /movies/:id
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}></Route>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="/movies" element={<Movies />}/>
+        <Route path="/movies/:id" element={<MovieDetail />}/>
+      </Route>
     </Routes>
   );
 }
