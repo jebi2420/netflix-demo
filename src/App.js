857a3ef951b626d4edd3ee27311dbc5c40871c74
jebi2420 <jebi2420@gmail.com>
2024-04-25 17:10:41 +0900

    [set] pages setting
    pages 폴더구조, 파일구조 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import AppLayout from './layout/AppLayout';
 
+// 홈페이지
+// 영화 전체 보여주는 페이지 (+ 서치)
+// 영화 디테일 페이지
 function App() {
   return (
     <Routes>
