1215efeb5950dd02ab98c0e3168846ede31b2eb8
jebi2420 <jebi2420@gmail.com>
2024-04-27 20:27:31 +0900

    [add] responsive 추가

--- src/pages/Homepage/component/PopularMovieslide/PopularMovieSlide.jsx
+++ src/pages/Homepage/component/PopularMovieslide/PopularMovieSlide.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies'
 
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 8
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
 const PopularMovieSlide = () => {
 
     const { data, isLoading, isError, error} = usePopularMoviesQuery();  
@@ -16,22 +31,11 @@ const PopularMovieSlide = () => {
     <div>
       <h3>Popular Movies</h3>
       <Carousel
-        swipeable={false}
-        draggable={false}
-        showDots={true}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        autoPlay={this.props.deviceType !== "mobile" ? true : false}
-        autoPlaySpeed={1000}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        deviceType={this.props.deviceType}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
+        infinite={true} // 무한반복 여부
+        centerMode={true} // 가운데 놓을거냐
+        itemClass="movie-slider p-1" // slide item class
+        containerClass="carousel-container" 
+        responsive={responsive} // responsive 설정을 해야 화면에 따른 크기 설정 가능
         >
         <div>Item 1</div>
         <div>Item 2</div>
