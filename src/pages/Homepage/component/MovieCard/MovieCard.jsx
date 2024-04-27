eb985045f868c0f5544d28ed1e6f4435417016c9
jebi2420 <jebi2420@gmail.com>
2024-04-27 22:27:17 +0900

    [add] MovieCard - css 작업중

--- src/pages/Homepage/component/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/component/MovieCard/MovieCard.jsx
@@ -9,16 +9,18 @@ const MovieCard = ({movie}) => {
         backgroundImage: 
           "url("+`https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`+")"
       }} 
-      className='movie-card' 
+      className="movie-card"
     >
-      <h1>{movie.title}</h1>
-      {movie.genre_ids.map((id) => 
-        (<Badge bg="danger">{id}</Badge>
-      ))}
-      <div>
-        <div>{movie.vote_average}</div>
-        <div>{movie.popularity}</div>
-        <div>{movie.adult?'over18':'under18'}</div>
+      <div className="overlay">
+        <h1>{movie.title}</h1>
+        {movie.genre_ids.map((id) =>
+          (<Badge bg="danger">{id}</Badge>
+        ))}
+        <div>
+          <div>{movie.vote_average}</div>
+          <div>{movie.popularity}</div>
+          <div>{movie.adult?'over18':'under18'}</div>
+        </div>
       </div>
     </div>
   )
