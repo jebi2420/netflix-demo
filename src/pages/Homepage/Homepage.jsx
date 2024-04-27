ba85ff0017f281eb13c2ced7762da09e869b82b0
jebi2420 <jebi2420@gmail.com>
2024-04-27 14:34:59 +0900

    [add] 주석추가

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+// 1. 배너 => popular 영화를 들고와서 첫번째 아이템을 보여주자
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
+
 const Homepage = () => {
   return (
     <div>
