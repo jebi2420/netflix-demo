bf364814bda56b0058098668f108f5af4e809acf
jebi2420 <jebi2420@gmail.com>
2024-04-27 20:29:03 +0900

    [add] slide 추가

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Banner from './component/Banner/Banner'
+import PopularMovieSlide from './component/PopularMovieslide/PopularMovieSlide'
 
 // 1. 배너 => popular 영화를 들고와서 첫번째 아이템을 보여주자
 // 2. popular movie
@@ -10,6 +11,7 @@ const Homepage = () => {
   return (
     <div>
       <Banner />
+      <PopularMovieSlide />
     </div>
   )
 }
