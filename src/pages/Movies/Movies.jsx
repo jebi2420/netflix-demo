e3a0ba6cf1810dc1622c54844e2ee0288c636138
jebi2420 <jebi2420@gmail.com>
2024-04-28 19:48:50 +0900

    [add] 주석 추가

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// Movies 페이지로 올 수 있는 2가지 경로
+// 1. navbar에서 클릭해서 온 경우 => popularMovie 보여주기
+// 2. keyword를 입력해서 온 경우 => keyword와 관련된 영화들을 보여줌
 const Movies = () => {
   return (
     <div>
