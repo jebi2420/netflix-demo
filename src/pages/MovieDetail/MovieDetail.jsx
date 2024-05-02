ed433b91e1a199438336c9ac0c79ee83586040c2
jebi2420 <jebi2420@gmail.com>
2024-05-02 18:53:16 +0900

    [add] MovieDetail UI 작업

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -50,13 +50,13 @@ const MovieDetail = () => {
           backgroundImage:
           `linear-gradient(to left, rgba(0 0 0 / 20%), rgba(0, 0, 0, 1)), 
             linear-gradient(to bottom, rgba(0, 0, 0, 0.45)75%, rgba(0, 0, 0, 1)95%),
-            url(https://media.themoviedb.org/t/p/w600_and_h900_bestv2${data.backdrop_path})` 
+            url(https://media.themoviedb.org/t/p/w1920_and_h800_bestv2${data.backdrop_path})` 
       }} 
       >
         <div className='movie-poster'>
           <img 
             style={{
-              width: "100%"
+              width: "100%",
             }}
             src={`https://media.themoviedb.org/t/p/w600_and_h900_bestv2/${data.poster_path}`} 
             alt="" 
@@ -87,11 +87,17 @@ const MovieDetail = () => {
             <h3>Score</h3>
             <div className="sub-facts">
               <div className='sub-fact'>
+                <h5>rating</h5>
                 <FontAwesomeIcon icon={faStar} style={{color: "#FFD43B"}} />
-                {data.vote_average.toFixed(1)}
+                &nbsp;{data.vote_average.toFixed(1)}
               </div>
-              <div className='sub-fact'><FontAwesomeIcon icon={faFire} style={{color: "#d72e14",}} />{data.popularity}</div>
-              <div className='sub-fact'>${formatNumber(data.revenue)}</div>
+              <div className='sub-fact'>
+                <h5>Popularity</h5>
+                <FontAwesomeIcon icon={faFire} style={{color: "#d72e14",}} />&nbsp;{data.popularity}</div>
+              <div className='sub-fact'>
+                <h5>revenue</h5>
+                ${formatNumber(data.revenue)}
+              </div>
             </div>
           </div>
         </div>
