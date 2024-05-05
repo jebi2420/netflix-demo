12c00fca04c86fa98db6dcc188e9d7ea406db2b1
jebi2420 <jebi2420@gmail.com>
2024-05-05 17:36:48 +0900

    [add] 더보기 기능 추가

--- src/pages/Homepage/component/MovieReviews/MovieReviews.jsx
+++ src/pages/Homepage/component/MovieReviews/MovieReviews.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser, faStar} from '@fortawesome/free-solid-svg-icons'
 import './MovieReviews.css' 
+import { useState } from 'react'
 
 const MovieReviews = ({result}) => {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
   return (
     <div>
         <div className="user-review">
@@ -16,7 +23,17 @@ const MovieReviews = ({result}) => {
                 &nbsp;{result?.author_details.rating}</div>
             </div>
             <div className='movie-content-area'>
-                <div className='movie-content'>{result.content}</div>
+                <div 
+                    className='movie-content'
+                    style={{
+                        height: isExpanded ? 'auto' : 'calc(1.7em * 7)',
+                        overflow: isExpanded ? 'visible' : 'hidden',
+                        display: isExpanded ? 'inline-block' : '-webkit-box'
+                    }}
+                >
+                    {result.content}
+                </div>
+                <button className='read-more-btn' onClick={toggleExpand}>{isExpanded ? 'less' : 'read more'}</button>
             </div>
         </div>
     </div>
