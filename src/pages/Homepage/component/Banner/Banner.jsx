d14d5cdf5c704f50052258c6ddfda337fa12c74e
jebi2420 <jebi2420@gmail.com>
2024-05-12 16:17:24 +0900

    [add] banner 클릭시 해당 영화 이동

--- src/pages/Homepage/component/Banner/Banner.jsx
+++ src/pages/Homepage/component/Banner/Banner.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies'
 import Alert from 'react-bootstrap/Alert';
 import './Banner.css'
-import MovieVideo from '../../../../common/MovieVideo/MovieVideo';
 import MovieModal from '../../../../common/Modal/MovieModal';
 import { useMovieVideosQuery } from '../../../../hooks/useMovieVideos';
+import { useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+  const navigate = useNavigate();
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
   const movieId = data?.results[0]?.id;
+  
   console.log("movie", movieId)
+
+
   const {
     data:videoData,
     isLoading: isVideoLoading,
@@ -28,6 +32,18 @@ const Banner = () => {
     )
   }
 
+  const goToDetail = (id) => {
+    // 현재 경로 확인
+    const currentPath = window.location.pathname;
+    //목표 경로 생성
+    const targetPath = `/movies/${id}`;
+
+    // 현재 경로와 목표 경로가 다를 경우에만 navigate 실행
+    if(currentPath !== targetPath){
+      navigate(targetPath);
+    }
+  }
+
   return (
     <div 
       style={{
@@ -36,6 +52,7 @@ const Banner = () => {
           `https://media.themoviedb.org/t/p/w1920_and_h800_bestv2${data?.results[0].backdrop_path}` + ")"
       }}
       className='banner'  
+      onClick={() => goToDetail(movieId)}
     >
       <div className="text-white banner-text-area">
         <div className="banner-title">
