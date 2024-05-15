4e1003e44764b4f4d4a9659dae81fde5139458b3
jebi2420 <jebi2420@gmail.com>
2024-05-15 16:57:03 +0900

    [add] banner mobile css

--- src/pages/Homepage/component/Banner/Banner.jsx
+++ src/pages/Homepage/component/Banner/Banner.jsx
@@ -7,7 +7,7 @@ import { useMovieVideosQuery } from '../../../../hooks/useMovieVideos';
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFire } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingSpinner from '../../../../common/Spinner/Spinner';
 
 const Banner = () => {
@@ -15,7 +15,29 @@ const Banner = () => {
   const navigate = useNavigate();
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
   const movieId = data?.results[0]?.id;
-  
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 48rem)');
+
+    const handleMediaChange = (e) => {
+      if (e.matches) {
+        setBackgroundImage(`https://media.themoviedb.org/t/p/w600_and_h900_bestv2${data?.results[0].backdrop_path}`);
+      } else {
+        setBackgroundImage(`https://media.themoviedb.org/t/p/w1920_and_h800_bestv2${data?.results[0].backdrop_path}`);
+      }
+    };
+
+    // 처음 로드 시 미디어 쿼리 상태를 확인
+    handleMediaChange(mediaQuery);
+
+    // 미디어 쿼리 상태가 변경될 때마다 호출
+    mediaQuery.addEventListener('change', handleMediaChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaChange);
+    };
+  }, [data]);
 
   const {
     data:videoData,
@@ -53,12 +75,14 @@ const Banner = () => {
     }
   }
 
+  // https://media.themoviedb.org/t/p/w1920_and_h800_bestv2${data?.results[0].backdrop_path}
+
   return (
     <div 
       style={{
         backgroundImage: 
           "url(" +
-          `https://media.themoviedb.org/t/p/w1920_and_h800_bestv2${data?.results[0].backdrop_path}` + ")"
+          `${backgroundImage}` + ")"
       }}
       className='banner'  
     >
